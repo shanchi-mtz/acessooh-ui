d6017c3abcb260a9ec0c69648d54d76baad5ed71
shanchi2 <raphaelspag@gmail.com>
2025-09-01 16:04:24 -0300

    fix: ajuste no vite.config para deploy do git

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/acessooh-ui/", // 👈 apenas o nome do repo entre barras
-});
+  base: '/acessooh-ui/', // 👈 garante que o GitHub Pages funcione sem mexer no index.html
+})
