83944a50a0856dc07bec8d87b845ae534a56d000
shanchi2 <raphaelspag@gmail.com>
2025-09-02 18:02:09 -0300

    chore: publish dist

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/acessooh-ui/",  // 👈 obrigatório no GitHub Pages
-});
